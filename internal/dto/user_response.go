237d9536f7b07d80eadb8f1d536a42a516b144c0
safaksonmez <drapius@hotmail.com>
2024-06-02 17:22:52 +0300

    fix: response changed for gateway.

--- internal/dto/user_response.go
+++ internal/dto/user_response.go
@@ -7,19 +7,9 @@ import (
 )
 
 type UserLoginRequestResponse struct {
-	ID            string                       `json:"id"`
-	Name          string                       `json:"name"`
-	Surname       string                       `json:"surname"`
-	Email         string                       `json:"email"`
-	PhoneNumber   string                       `json:"phone_number"`
-	Role          string                       `json:"role"`
-	CreatedAt     time.Time                    `json:"created_at"`
-	AccessToken   string                       `json:"access_token"`
-	AccessPublic  string                       `json:"access_public"`
-	RefreshToken  string                       `json:"refresh_token"`
-	RefreshPublic string                       `json:"refresh_public"`
-	ExpiredAt     time.Time                    `json:"expired_at"`
-	Attributes    map[string]entity.Permission `json:"attributes"`
+	Token     string                `json:"token"`
+	PublicKey string                `json:"public_key"`
+	User      *aggregate.UserAccess `json:"user"`
 }
 
 type GetUserRequestResponse struct {
@@ -35,19 +25,9 @@ type GetUserRequestResponse struct {
 
 func NewUserLoginRequestResponse(userData *aggregate.UserAccess) *UserLoginRequestResponse {
 	return &UserLoginRequestResponse{
-		ID:            userData.ID,
-		Name:          userData.Name,
-		Surname:       userData.Surname,
-		Email:         userData.Email,
-		PhoneNumber:   userData.PhoneNumber,
-		Role:          string(userData.Role),
-		Attributes:    userData.Attributes,
-		CreatedAt:     userData.CreatedAt,
-		AccessToken:   userData.AccessToken,
-		AccessPublic:  userData.AccessPublic,
-		RefreshToken:  userData.RefreshToken,
-		RefreshPublic: userData.RefreshPublic,
-		ExpiredAt:     userData.ExpiredAt,
+		Token:     userData.AccessToken,
+		PublicKey: userData.AccessPublic,
+		User:      userData,
 	}
 }
 

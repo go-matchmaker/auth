8114c07f8090683aab788bc06766cc6c0fe691d3
bulutcan99 <gocerbulutcan@gmail.com>
2024-06-03 17:38:23 +0300

    refactor: dto and model seperated to converter

--- internal/converter/model_converter.go
+++ internal/converter/model_converter.go
@@ -2,6 +2,7 @@ package converter
 
 import (
 	"auth/internal/core/domain/aggregate"
+	"auth/internal/core/domain/entity"
 	"auth/internal/dto"
 )
 
@@ -9,7 +10,7 @@ func UserLoginModelToDto(userData *aggregate.UserAccess) *dto.UserLoginRequestRe
 	return &dto.UserLoginRequestResponse{
 		Token:     userData.AccessToken,
 		PublicKey: userData.AccessPublic,
-		User:      userData,
+		User:      (*dto.UserAccess)(userData),
 	}
 }
 
@@ -23,6 +24,24 @@ func GetUserModelToDto(userData *aggregate.UserAggregate) *dto.GetUserResponse {
 		Role:         string(userData.User.Role),
 		CreatedAt:    userData.User.CreatedAt,
 		DepartmentID: userData.Department.ID,
-		Attributes:   userData.Permissions,
+		Attributes:   userPermissionsModelToDto(userData.Permissions),
 	}
+}
+
+func userPermissionsModelToDto(permissions map[string]*entity.Permission) map[string]*dto.Permission {
+	result := make(map[string]*dto.Permission)
+	for key, value := range permissions {
+		result[key] = &dto.Permission{
+			View:        value.View,
+			Search:      value.Search,
+			Detail:      value.Detail,
+			Add:         value.Add,
+			Update:      value.Update,
+			Delete:      value.Delete,
+			Export:      value.Export,
+			Import:      value.Import,
+			CanSeePrice: value.CanSeePrice,
+		}
+	}
+	return result
 }
--- internal/dto/user_response.go
+++ internal/dto/user_response.go
@@ -1,25 +1,43 @@
 package dto
 
 import (
-	"auth/internal/core/domain/aggregate"
-	"auth/internal/core/domain/entity"
 	"time"
 )
 
 type UserLoginRequestResponse struct {
-	Token     string                `json:"token"`
-	PublicKey string                `json:"public_key"`
-	User      *aggregate.UserAccess `json:"user"`
+	Token     string      `json:"token"`
+	PublicKey string      `json:"public_key"`
+	User      *UserAccess `json:"user"`
+}
+
+type UserAccess struct {
+	ID            string `json:"id"`
+	AccessToken   string `json:"access_token"`
+	AccessPublic  string `json:"access_public"`
+	RefreshToken  string `json:"refresh_token"`
+	RefreshPublic string `json:"refresh_public"`
 }
 
 type GetUserResponse struct {
-	ID           string                        `json:"id"`
-	Name         string                        `json:"name"`
-	Surname      string                        `json:"surname"`
-	Email        string                        `json:"email"`
-	PhoneNumber  string                        `json:"phone_number"`
-	Role         string                        `json:"role"`
-	DepartmentID string                        `json:"department_id"`
-	Attributes   map[string]*entity.Permission `json:"attributes"`
-	CreatedAt    time.Time                     `json:"created_at"`
+	ID           string                 `json:"id"`
+	Name         string                 `json:"name"`
+	Surname      string                 `json:"surname"`
+	Email        string                 `json:"email"`
+	PhoneNumber  string                 `json:"phone_number"`
+	Role         string                 `json:"role"`
+	DepartmentID string                 `json:"department_id"`
+	Attributes   map[string]*Permission `json:"attributes"`
+	CreatedAt    time.Time              `json:"created_at"`
+}
+
+type Permission struct {
+	View        bool `json:"view"`
+	Search      bool `json:"search"`
+	Detail      bool `json:"detail"`
+	Add         bool `json:"add"`
+	Update      bool `json:"update"`
+	Delete      bool `json:"delete"`
+	Export      bool `json:"export"`
+	Import      bool `json:"import"`
+	CanSeePrice bool `json:"can_see_price"`
 }
